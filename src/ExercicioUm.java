031cddf1b15df017a7ffff0d2085e5d9bc2c56fe
TabataGrigaitis <tabatagrigaitis@gmail.com>
2020-03-11 21:16:22 -0300

    codificação do método possoIrBanco()

--- src/ExercicioUm.java
+++ src/ExercicioUm.java
@@ -3,9 +3,12 @@ public class ExercicioUm {
 	
 	public static boolean ehDiaSemana(String dia) {
 
-		boolean ehdia =(dia=="segunda" || dia=="terça"|| dia=="quarta"|| dia=="quinta"|| dia=="sexta"|| dia=="sabado"|| dia=="domingo"); 
+		boolean ehdia =(dia=="segunda" || dia=="terça"|| dia=="quarta"|| dia=="quinta"|| dia=="sexta"); 
 		return ehdia;
-				
+		//ou 
+		//if(dia=="domingo"||dia=="sabado")
+		//{return false;}
+		//else{return true}
 	}
 	
 public static void main(String[] args) {
